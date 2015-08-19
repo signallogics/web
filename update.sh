595e948fa26b721d89adac32d79787cd3c585d78
Evgenii Sopov <eso@startext.tomsk.ru>
2015-08-19 23:48:22 +0600

    updated update.sh

--- update.sh
+++ update.sh
@@ -7,8 +7,11 @@ git pull
 npm install
 gulp --production
 
-# kill process
-npm stop --production
+# TODO: need kill all node process
+# TODO: need kill all mongod process
 
-# start
-npm start --production
+# start mongod
+(mongod > mongod.log &> error_mongod.log &)
+
+# start site
+(npm start --production > start.log &> error.log &)
